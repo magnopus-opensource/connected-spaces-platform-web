5964377041ccacff7f56108896244d5c3c175839
Elliot Morris <Elliot.Morris@magnopus.com>
2026-06-27 17:22:03 +0100

    Switch C++ editor toolchain to clangd (#20)
    * Add clangd support
    
    We (I) want this to support the clangd extension in VS Codium, as microsoft forbids anything but microsoft products to use their visual C++ extension. Given that VS Code is degrading by the day this seems sensible, and it's more cross platform than relying on the microsoft toolchain.
    
    It also has the advantages of being the native toolchain that our C++ formatter uses.
    
    * More single line formatting style
    
    Slightly alter our formatting style to allow more single-line statements.
    
    Some may balk at this, but the reasoning is that this is a declarative codebase, there are going to be long lists of declarations that are all self similar, hundreds of the things ... maybe thousands. The difference between dedicating a full 50% of the lines in those files to curly braces will add up fast.
    
    * Recommend clang tools + set default formatter
    
    I see no reason to recommend the microsoft C++ tools over the open clang tooling, which is available everywhere.
    
    * Apply reformatting to all C++ source
    
    This should get us on the same page and stop us doing whitespace reformats constantly.
    
    As with all auto-formatting, I don't love all of it, but the tradeoff for having an automatically applied standard is worth it.
    
    * Enable warnings as errors.
    
    Initially broke this out into a seperate branch, but as there are currently no warnings in the source (honestly sort of surprised), might as well just do it here.

--- src/bindings/utils/Handles.h
+++ src/bindings/utils/Handles.h
@@ -8,12 +8,15 @@ namespace bindings::utils {
 /*
  * Check to see if we're a C++ bound embind object or not.
  * `isDeleted` is technically not a contract check, embind doesn't
- * provide something altogether straightforward for this, but this 
+ * provide something altogether straightforward for this, but this
  * is as good as standard.
  */
-inline bool IsBoundHandle(emscripten::val v) {
-    if (v.typeOf().as<std::string>() != "object") return false;
-    if (v.isNull()) return false;
+inline bool IsBoundHandle(emscripten::val v)
+{
+    if (v.typeOf().as<std::string>() != "object")
+        return false;
+    if (v.isNull())
+        return false;
     return !v["isDeleted"].isUndefined();
 }
 
