1bc9a9396f956de6273b6de788173ebc41f95071
MAG-ThomasGreenhalgh <111896829+MAG-ThomasGreenhalgh@users.noreply.github.com>
2026-07-09 11:18:39 +0100

    Add support for optional of types with custom disposable behaviour, such as array, list and map containers

--- src/bindings/containers/Optional.h
+++ src/bindings/containers/Optional.h
@@ -33,4 +33,37 @@ template <typename T> struct BindingType<csp::common::Optional<T>> {
     }
 };
 
+/*
+ * Return-path binding for optional with types that need disposable behaviour.
+ * Combines optional semantics (value or undefined) with [Symbol.dispose] attachment.
+ *
+ * A binding for JSDisposable<T> must exist for this to work.
+ *
+ * Example usage in binding site:
+ *   .function("getOptionalList", +[](const MyType& self) {
+ *       return bindings::utils::JSDisposable<csp::common::Optional<csp::common::List<T>>>{self.GetOptionalList()};
+ *   })
+ *
+ * Registration:
+ *   emscripten::register_type<bindings::utils::JSDisposable<csp::common::Optional<csp::common::List<T>>>>("(T[] & Disposable) | undefined");
+ */
+template <typename T> struct BindingType<bindings::utils::JSDisposable<csp::common::Optional<T>>> {
+    using ValBinding = BindingType<val>;
+    using InnerDisposableBinding = BindingType<bindings::utils::JSDisposable<T>>;
+    using WireType = typename ValBinding::WireType;
+
+    static WireType toWireType(const bindings::utils::JSDisposable<csp::common::Optional<T>>& wrapper, rvp::default_tag)
+    {
+        const auto& opt = wrapper.view;
+
+        if (!opt.HasValue()) {
+            return ValBinding::toWireType(val::undefined(), rvp::default_tag {});
+        }
+
+        // Wrap the inner value in a JSDisposable and delegate to its BindingType
+        bindings::utils::JSDisposable<T> innerWrapper(*opt);
+        return InnerDisposableBinding::toWireType(innerWrapper, rvp::default_tag {});
+    }
+};
+
 }
