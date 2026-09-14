b963431e6ab742b1a3c2a029e99b47095b389798
MAG-ThomasGreenhalgh <111896829+MAG-ThomasGreenhalgh@users.noreply.github.com>
2026-09-14 17:10:39 +0100

    Remove redundant Bindings.cpp file as we now have bindings for CSPFoundation. I've left the version test in for now. We can remove it when implementing integration tests.

--- tests/src/version.test.ts
+++ tests/src/version.test.ts
@@ -2,7 +2,6 @@ import { describe, it, expect, beforeAll } from 'vitest';
 import { loadCSP } from '../loadModule';
 import type { MainModule } from 'connected-spaces-platform-bindings';
 
-
 /*
  * This is only a temporary test to demonstrate that an actual CSP method can be bound.
  * It will be deleted when we begin wallpapering the API proper.
@@ -16,11 +15,11 @@ describe('CSPFoundation', () => {
   });
 
   it('exports GetVersion', () => {
-    expect(typeof csp.GetVersion).toBe('function');
+    expect(typeof csp.CSPFoundation.getVersion).toBe('function');
   });
 
   it('returns a non-empty version string', () => {
-    const version = csp.GetVersion();
+    const version = csp.CSPFoundation.getVersion();
     expect(typeof version).toBe('string');
     expect(version.length).toBeGreaterThan(0);
   });
